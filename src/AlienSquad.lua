e4cb6d70db2a615b0a64f3deeae739c8b994e86e
Oliver De Jesús Rosario Reyes <under-blade500@hotmail.com>
2020-09-03 20:15:49 -0400

    merged from squad_movement

--- src/AlienSquad.lua
+++ src/AlienSquad.lua
@@ -4,28 +4,52 @@ function AlienSquad:init()
     self.aliens = {}
 
     --Insert empty tables to later fill them with aliens
-    for i = 1, 4, 1 do
+    for i = 1, SQUAD_ROWS, 1 do
         table.insert(self.aliens, {})
     end
 
-    for y = 1, 4, 1 do
-        for x = 1, 10, 1 do 
+    for y = 1, SQUAD_ROWS, 1 do
+        for x = 1, SQUAD_COLUMNS, 1 do             
             table.insert(self.aliens[y], Alien(x, y))
         end
     end
+    self.movementDirection = "right"
 
-    self.aliensAlive = 40    
+    self.numAliensAlive = SQUAD_TOTAL_UNITS
     self.bullets = {}
 end
 
-function AlienSquad:update(dt)
-    --Update the position of the ship
-    for y = 1, 4, 1 do
-        for x = 1, 10, 1 do 
-            self.aliens[y][x]:update(dt)
+function AlienSquad:update(dt)    
+    --Update Aliens:
+    --Set by how much the aliens should move (the x movement gets faster as the number of aliens decreases)
+    local aliensDx = ALIEN_X_SPEED + (SQUAD_TOTAL_UNITS - self.numAliensAlive)
+    local aliensDy = 0
+
+    --Get the left-most or right-most alien in the squad    
+    local outermost = AlienSquad:getOutermostAlien(self.movementDirection, self.aliens)
+    if ((outermost.x + outermost.width) >= VIRTUAL_WIDTH) then
+        self.movementDirection = "left"
+        aliensDy = ALIEN_Y_SPEED
+    elseif outermost.x <= 0 then
+        self.movementDirection = "right"
+        aliensDy = ALIEN_Y_SPEED
+    end
+
+    if self.movementDirection == "right" then
+        for y = 1, SQUAD_ROWS, 1 do
+            for x = 1, SQUAD_COLUMNS, 1 do 
+                self.aliens[y][x]:update(dt, aliensDx, aliensDy)
+            end
+        end
+    else
+         for y = 1, SQUAD_ROWS, 1 do
+            for x = 1, SQUAD_COLUMNS, 1 do 
+                self.aliens[y][x]:update(dt, -aliensDx, aliensDy)
+            end
         end
     end
 
+    --Update bullets:
     --If the bullets table is not empty, update the position of the bullets
     if table.getn(self.bullets) >= 1 then
         for index, bullet in pairs(self.bullets) do
@@ -35,22 +59,47 @@ function AlienSquad:update(dt)
             end
         end
     end
+
+end
+
+function AlienSquad:getOutermostAlien(movementDirection, aliens)
+    if movementDirection == "right" then
+        for x=SQUAD_COLUMNS, 1, -1 do
+            for y = SQUAD_ROWS, 1, -1 do 
+                local alien = aliens[y][x]
+                if alien.alive then
+                    return alien
+                end
+            end
+        end
+    elseif movementDirection == "left" then
+        for x = 1, SQUAD_COLUMNS, 1 do
+            for y = 1, SQUAD_ROWS, 1 do    
+                local alien = aliens[y][x]
+                if alien.alive then
+                    return alien
+                end
+            end
+        end
+    end
+    
 end
 
 function AlienSquad:checkCollision(bullet)
-    for y = 1, 4, 1 do
-        for x = 1, 10, 1 do 
+    for y = 1, SQUAD_ROWS, 1 do
+        for x = 1, SQUAD_COLUMNS, 1 do 
             --Check if an alien is alive before checking for a collision for eficiency
             if self.aliens[y][x].alive then
                 --Check for a collition between the bullet and the alien; if they collide kill the alien and return true
                 if self.aliens[y][x]:collides(bullet) then
                     self.aliens[y][x].alive = false
-                    self.aliensAlive = self.aliensAlive - 1
+                    self.numAliensAlive = self.numAliensAlive - 1
                     return true
                 end
             end
         end
     end
+
 end
 
 function AlienSquad:shoot() 
@@ -59,16 +108,16 @@ function AlienSquad:shoot()
     if table.getn(self.bullets) >= 3 then
         return
     end
-    local shooter = self.aliens[math.random(1,4)][math.random(1,10)]
+    local shooter = self.aliens[math.random(1,SQUAD_ROWS)][math.random(1,SQUAD_COLUMNS)]
     if shooter.alive then
-        table.insert(self.bullets, Bullet(shooter.renderX + (shooter.width/2), shooter.renderY, "DOWN"))
+        table.insert(self.bullets, Bullet(shooter.x + (shooter.width/2), shooter.y, "DOWN"))
     end
 end
 
 function AlienSquad:render()
     --Render the aliens
-    for y = 1, 4, 1 do
-        for x = 1, 10, 1 do 
+    for y = 1, SQUAD_ROWS, 1 do
+        for x = 1, SQUAD_COLUMNS, 1 do 
             self.aliens[y][x]:render()
         end
     end
